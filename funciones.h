b1195db4ad6b669f950f548774780efe23e81139
JonnyJoan <57011841+JonnyJoan@users.noreply.github.com>
2019-11-01 19:37:38 -0500

    Bosquejo de código

---
+++ funciones.h
@@ -0,0 +1,20 @@
+#ifndef PROYECTOJOAN_FUNCIONES_H
+#define PROYECTOJOAN_FUNCIONES_H
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+void ValidarNumRobots();
+void LlenarMapa();
+void ImprimirMatriz();
+void ColocarObjetos();
+void DarPosicion();
+void GenerarRobots();
+void ValidarEleccion();
+void MenuDeOperaciones();
+void IngresarObjeto();
+void RetirarObjeto();
+void Operacion();
+
+#endif //PROYECTOJOAN_FUNCIONES_H
