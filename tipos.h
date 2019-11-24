20395abc6c9c35e6b9f0b101f0b7975eed908e1d
jorgecollazos <57011579+jorgecollazos@users.noreply.github.com>
2019-11-23 22:52:43 -0500

    Actualización tipos genericos

---
+++ tipos.h
@@ -0,0 +1,17 @@
+//
+// Created by NINCOL on 1/11/2019.
+//
+
+#ifndef PROYECTO_POOX_TIPOS_H
+#define PROYECTO_POOX_TIPOS_H
+#include <cstddef>
+#include <string>
+#include <vector>
+using namespace std;
+// Definiendo
+using TipoEntero = int;
+using TipoCaracter = char;
+using TipoString = string;
+using TipoDouble = double;
+using posicion = int;
+#endif //PROYECTO_POOX_TIPOS_H
