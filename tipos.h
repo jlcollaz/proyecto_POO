02d510f0328f295c4e5ae82c351d01593964c5bc
jorgecollazos <57011579+jorgecollazos@users.noreply.github.com>
2019-11-25 09:05:56 -0500

    Actualización de tipos.h

---
+++ tipos.h
@@ -0,0 +1,21 @@
+//
+// Created by NINCOL on 1/11/2019.
+//
+
+#ifndef PROYECTO_POOX_TIPOS_H
+#define PROYECTO_POOX_TIPOS_H
+#include <cstddef>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
+#include <ostream>
+using namespace std;
+// Definiendo
+using TipoEntero = int;
+using TipoCaracter = char;
+using TipoString = string;
+using TipoCaracter = char;
+using TipoDouble = double;
+using posicion = int;
+#endif //PROYECTO_POOX_TIPOS_H
